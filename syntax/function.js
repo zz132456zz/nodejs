a8964532a5bd171477e10d176a49e6be83496b9e
zz132456zz <zz132456zz@kakao.com>
2020-11-06 19:33:50 +0900

    javascript syntax

---
+++ syntax/function.js
@@ -0,0 +1,23 @@
+f123();
+console.log('A');
+console.log('B');
+console.log('Z');
+f123();
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+console.log('U');
+f123();
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+f123();
+
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
